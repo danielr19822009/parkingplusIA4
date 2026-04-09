9af3cd33940a106761f0506f8a5ec7e22da7a955
Daniel <danielr19822009@gmail.com>
2026-04-09 09:32:43 -0500

    se modifica .env para conexion con la bd cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_ADDON_HOST || process.env.DB_HOST,
+  user: process.env.MYSQL_ADDON_USER || process.env.DB_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB || process.env.DB_NAME,
+  port: process.env.MYSQL_ADDON_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
